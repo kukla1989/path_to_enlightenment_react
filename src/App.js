8da29d203b5e74771f808185d1d0bc0bad23cff1
kukla1989 <kukla.1989@ukr.net>
2020-01-07 13:23:39 +0200

    base and first try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
+import lampOn from './lamp_on.jpeg';
+import Footer from './Footer.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img id="lamp" src={lampOn} alt = 'lamp on' width="20%" />
+     <button onclick="document.getElementById("lamp").src={lampOn}"> turn off </button>
+      <h1> hello world, try 1 </h1> 
+    <Footer />
+
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+
+
+function Footer() {
+const styleFooter = {
+	backgraundColor: "#0000CD"
+}
+return (
+	<div>
+
+	</div>
+	)
+
+}
+
+
+
+export default Footer
+
+
+{/*		{document.body.style = 'background: red;'}  {{backgraundColor: "#0000CD"} 
+
+
+
+<h3 style={{backgraundColor: 'blue'}}>  made by kukla </ h3>
+
+
+
+*/}
+
